084443530ba18f63e5e0c5c8e1036dec02648645
Annekathrin Silvia Ludt <annekathrin@zdv-vpn01-38-177.zdv.Uni-Mainz.DE>
2022-12-16 14:35:34 +0100

    Added tests and documentation

--- R/clustering.R
+++ R/clustering.R
@@ -1,66 +1,73 @@
-#' Title
+#' Check for subset inclusion
 #'
-#' @param seeds
+#' Check if every set is unique and there are no subset included in larger sets
 #'
-#' @return
+#' @param seeds A `list` of sets of numerical values
+#'
+#' @return A `list` of unique sets
 #' @export
+#' @import rje
 #'
 #' @examples
+#' seeds <- list(c(1:5), c(2:5), c(6:10))
+#' s <- checkInclusion(seeds)
 checkInclusion <- function(seeds) {
   remove <- c()
-  if(length(seeds) < 2){
+  if (length(seeds) < 2) {
     return(seeds)
   }
   for (i in 1:(length(seeds) - 1)) {
     s1 <- seeds[[i]]
-    l1 <- length(s1)
     for (j in (i + 1):length(seeds)) {
       s2 <- seeds[[j]]
-      l2 <- length(s2)
-      if (l1 < l2) {
-        if (length(intersect(s1, s2)) == l1) {
-          remove <- c(remove, i)
-          break
-        }
-      } else{
-        if (length(intersect(s2, s1)) == l2) {
-          remove <- c(remove, j)
-        }
+      if (rje::is.subset(s1, s2)) {
+        remove <- c(remove, i)
+      } else if (rje::is.subset(s2, s1)) {
+        remove <- c(remove, j)
       }
     }
   }
   remove <- unique(remove)
-  if(length(remove) == 0){
+  if (length(remove) == 0) {
     return(seeds)
-  }else{
+  } else{
     return(seeds[-remove])
   }
 }
 
-#' Title
+#' Find clustering seeds
 #'
-#' @param distances
-#' @param simThreshold
-#' @param memThreshold
+#' Find initial seeds for the clustering. #TODO: Verlinken auf die David publication
 #'
-#' @return
+#' @param distances A [Matrix::Matrix()] of (distance) scores
+#' @param simThreshold Numerical value, a threshold of what is considered a
+#'                     close relationsship between two elements in distances
+#' @param memThreshold Numerical value, a threshold used to identify members of
+#'                     a seeds
+#'
+#' @return A `list` of seeds which can be used for clustering
 #' @export
 #'
 #' @examples
-seedFinding <- function(distances, simThreshold, memThreshold){
-  # simthreshold: what is considered 'close' relationship
-  # memthreshold: how many members of a possible seed need a close relationship for the seed to be considered
+#' m <- Matrix::Matrix(runif(100, min = 0, max = 1), 10, 10)
+#' seeds <- seedFinding(distances = m, simThreshold = 0.3, memThreshold = 0.5)
+seedFinding <- function(distances, simThreshold, memThreshold) {
+  if (is.null(distances) || length(distances) == 0) {
+    return(NULL)
+  }
   seeds <- list()
 
-  # Build matrix with xij = 1 indicating i and j are close (sim(i, j) <= simThreshold)
-  reach <- apply(distances, 1, function(x) as.numeric(x <= simThreshold))
-  for(i in 1:nrow(distances)){
-    if(sum(reach[i,], na.rm = TRUE) >= 2){
-      members <- which(reach[i, ] == 1)
-      includethreshold <- (length(members)^2 - length(members)) * memThreshold
+  reach <-
+    apply(distances, 1, function(x)
+      as.numeric(x <= simThreshold))
+  for (i in 1:nrow(distances)) {
+    if (sum(reach[i, ], na.rm = TRUE) >= 2) {
+      members <- which(reach[i,] == 1)
+      includethreshold <-
+        (length(members) ^ 2 - length(members)) * memThreshold
       reach_red <- reach[members, members]
       in_reach <- sum(reach_red)
-      if(in_reach >= includethreshold){
+      if (in_reach >= includethreshold) {
         members <- c(members, i)
         seeds <- c(list(sort(members)), seeds)
       }
@@ -72,32 +79,36 @@ seedFinding <- function(distances, simThreshold, memThreshold){
 }
 
 
-#' Title
+#' Find cluster from initial seeds
 #'
-#' @param seeds
-#' @param threshold
+#' Find clusters from initial seeds according to (#TODO: David publication linken)
 #'
-#' @return
+#' @param seeds A `list` of seeds, e.g. determined by seedFinding
+#' @param threshold Numerical value, threshold for merging seeds
+#'
+#' @return A `list` of clusters
 #' @export
 #'
 #' @examples
-clustering <- function(seeds, threshold){
-  if(length(seeds) <= 1){
+#' seeds <- list(c(1:5), c(6:10))
+#' cluster <- clustering(seeds, 0.5)
+clustering <- function(seeds, threshold) {
+  if (length(seeds) <= 1) {
     return(seeds)
   }
   mergeable <- rep(TRUE, length(seeds))
-  while(any(mergeable)){
+  while (any(mergeable)) {
     index <- which(mergeable)[1]
-    if(index > length(seeds)){
+    if (index > length(seeds)) {
       break
     }
     s1 <- seeds[[index]]
     l <- length(seeds)
-    for(j in 1:length(seeds)){
+    for (j in 1:length(seeds)) {
       s2 <- seeds[[j]]
       int <- intersect(s1, s2)
       union <- union(s1, s2)
-      if(length(int) >= (threshold * length(union))){
+      if (length(int) >= (threshold * length(union))) {
         remove <- list(s1, s2)
         seeds <- seeds[!(seeds %in% remove)]
         seeds <- c(list(union), seeds)
@@ -106,7 +117,7 @@ clustering <- function(seeds, threshold){
         break
       }
     }
-    if(l == length(seeds)){
+    if (l == length(seeds)) {
       mergeable[[index]] <- FALSE
     }
 
@@ -114,31 +125,37 @@ clustering <- function(seeds, threshold){
   return(seeds)
 }
 
-#' Title
+#' Get Dataframe of Cluster
 #'
-#' @param cluster
-#' @param n_gs
+#' Get Dataframe from a `list` of clusters
 #'
-#' @return
+#' @param cluster A `list` of clusters
+#' @param geneset_names A vector of geneset names
+#'
+#' @return A `data.frame` mapping each geneset to the cluster(s) it belongs to
 #' @export
 #'
 #' @examples
-getClusterDatatable <- function(cluster, geneset_names){
+#' cluster <- list(c(1:5), c(4:8))
+#' geneset_names <- c("a", "b", "c", "d", "e", "f", "g", "h", "i")
+#' df <- getClusterDatatable(cluster, geneset_names)
+getClusterDatatable <- function(cluster, geneset_names) {
+  stopifnot(length(geneset_names) > 0)
   n_gs <- length(geneset_names)
   df <- vector("list", n_gs)
 
-  if(length(cluster) == 0){
+  if (length(cluster) == 0) {
     df <- data.frame(Cluster = rep("No associated Cluster", n_gs))
     rownames(df) <- geneset_names
     return(df)
   }
 
-  for(i in 1:length(cluster)){
-    for(j in cluster[[i]]){
+  for (i in 1:length(cluster)) {
+    for (j in cluster[[i]]) {
       entry <- df[[j]]
-      if(is.null(entry)){
+      if (is.null(entry)) {
         entry <- i
-      }else{
+      } else{
         entry <- c(entry, i)
       }
 
@@ -149,10 +166,14 @@ getClusterDatatable <- function(cluster, geneset_names){
   df <- data.frame(matrix(df, nrow = n_gs, ncol = 1))
   colnames(df) <- c("Cluster")
   cluster <- df$Cluster
-  cluster <- lapply(cluster, function(x) if(is.null(x)){"No associated Cluster"}else{x})
+  cluster <-
+    lapply(cluster, function(x)
+      if (is.null(x)) {
+        "No associated Cluster"
+      } else{
+        x
+      })
   df$Cluster <- cluster
   rownames(df) <- geneset_names
   return(df)
 }
-
-
--- tests/testthat/test-clustering.R
+++ tests/testthat/test-clustering.R
@@ -1,3 +1,67 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("checkInclusion - empty seeds", {
+  seeds <- checkInclusion(list())
+  expect_true(length(seeds) == 0)
+  expect_identical(seeds, list())
+})
+
+test_that("checkInclusion - only one set", {
+  seeds <- checkInclusion(list(c(1:20)))
+  expect_identical(seeds, list(c(1:20)))
+
+  seeds <- checkInclusion(list(c(1:20), c(1:20), c(1:20)))
+  expect_identical(seeds, list(c(1:20)))
+  expect_true(length(seeds) == 1)
+})
+
+test_that("checkInclusion runs correctly", {
+  initial_seeds <- list(c(1:5), c(1:3), c(6:7), c(6:10))
+  seeds <- checkInclusion(initial_seeds)
+  expect_true(length(seeds) < length(initial_seeds))
+  expect_identical(seeds, list(c(1:5), c(6:10)))
+  expect_true(length(seeds) == 2)
+})
+
+test_that("seedFinding - no distance scores", {
+  expect_true(is.null(seedFinding(list(), 0.3, 0.5)))
+  expect_true(is.null(seedFinding(NULL, 0.3, 0.5)))
+})
+
+test_that("seedFinding - no distances smaller simthreshold", {
+  distances <- Matrix::Matrix(0.9, 10, 10)
+  seeds <- seedFinding(distances, simThreshold = 0.3, memThreshold = 0.5)
+  expect_true(length(seeds) == 0)
+  expect_type(seeds, "list")
+})
+
+test_that("seedFinding runs correctly", {
+  distances <- Matrix::Matrix(0.9, 10, 10)
+  seeds <- seedFinding(distances, simThreshold = 0.9, memThreshold = 0.5)
+  expect_true(length(seeds) > 0)
+  expect_type(seeds, "list")
+})
+
+test_that("clustering - no seeds", {
+  expect_true(is.null(clustering(NULL, 0.5)))
+  expect_true(length(clustering(list(), 0.5)) == 0)
+})
+
+test_that("clustering runs correctly", {
+  seeds <- list(c(1:2), c(6:10), c(4:7))
+  cluster <- clustering(seeds, 0.5)
+  expect_true(length(cluster) <= length(seeds))
+  expect_true(length(cluster) > 0)
+  expect_type(cluster, "list")
+})
+
+test_that("getClusterDataTable - no geneset names", {
+  expect_error(getClusterDatatable(list(), list()))
+})
+
+test_that("getClusterDatatable runs correctly", {
+  geneset_names <- c("a", "b", "c", "d", "e", "f", "g", "h", "i")
+  df <- getClusterDatatable(list(), geneset_names)
+  expect_type(df, "list")
+  expect_true(nrow(df) == 9)
+  cluster <- list(c(1:4), c(5:8))
+  expect_type(getClusterDatatable(cluster, geneset_names), "list")
 })
