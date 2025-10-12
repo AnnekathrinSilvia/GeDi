1dbe68075536cc90c6889df965075afe1f164963
Federico Marini <marinif@uni-mainz.de>
2025-10-13 00:10:11 +0200

    adding documentation for the internal .map_to_color

--- R/gs_Graph.R
+++ R/gs_Graph.R
@@ -722,6 +722,36 @@ getGraphTitle <- function(geneset_df = NULL,
 }
 
 
+#' Maps numeric values to color values
+#'
+#' Maps numeric continuous values to values in a color palette
+#'
+#' @param x A character vector of numeric values (e.g. log2FoldChange values) to
+#' be converted to a vector of colors
+#' @param pal A vector of characters specifying the definition of colors for the
+#' palette, e.g. obtained via [RColorBrewer::brewer.pal()]
+#' @param symmetric Logical value, whether to return a palette which is symmetrical
+#' with respect to the minimum and maximum values - "respecting" the zero.
+#' Defaults to `TRUE`.
+#' @param limits A vector containing the limits of the values to be mapped. If
+#' not specified, defaults to the range of values in the `x` vector.
+#'
+#' @return A vector of colors, each corresponding to an element in the original
+#' vector
+#'
+#' @importFrom RColorBrewer brewer.pal
+#'
+#' @examples
+#' a <- 1:9
+#' pal <- RColorBrewer::brewer.pal(9, "Set1")
+#' map_to_color(a, pal)
+#' plot(a, col = map_to_color(a, pal), pch = 20, cex = 4)
+#'
+#' b <- 1:50
+#' pal2 <- grDevices::colorRampPalette(
+#'   RColorBrewer::brewer.pal(name = "RdYlBu", 11)
+#' )(50)
+#' plot(b, col = map_to_color(b, pal2), pch = 20, cex = 3)
 .map_to_color <- function (x, pal, symmetric = TRUE, limits = NULL)
 {
   if (is.null(limits)) {
